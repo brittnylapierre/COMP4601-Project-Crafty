6e634666c659b5b4aadc58017a633a21a611932a
Brittny Lapierre <lapierre@ca.ibm.com>
2018-04-12 18:51:34 -0400

    tore Deserres data

--- scraper.py
+++ scraper.py
@@ -13,14 +13,41 @@ import re
 url = "https://www.deserres.ca/en/brands"
 res =  requests.get(url);
 soup = BeautifulSoup(res.text, "lxml")
-#wraps = soup.find_all("div", {"class": "content-wrap"})
-#links = []
-#if wraps and len(wraps) > 0: 
-#	wrap = wraps[0]
-raw_links = soup.find_all("a", href=re.compile("brands"))
 
-links = []
+raw_links = soup.find_all("a", href=re.compile("brands\/"))
+
+brand_links = []
+product_links = [] 
+product_map = {}
+
 for a in raw_links:
-	if a['href'] not in links:
-		links.append(a['href'])
-print(links)
+	if a['href'] not in brand_links:
+		brand_links.append(a['href'])
+		res =  requests.get(a['href']+"?limit=all")
+		pageSoup = BeautifulSoup(res.text, "lxml")
+		product_headers = pageSoup.find_all("h2", {"class": "product-name"})
+		for h2 in product_headers:
+			raw_product_links = h2.find_all("a")
+			for product_link in raw_product_links:
+				if product_link['href'] not in product_links:
+					product_links.append(product_link['href'])
+					if product_link['data-price']:
+						product_map[product_link['href']] = {
+							'title': product_link.contents[0],
+							'description': "",
+							'price': product_link['data-price'],
+							'url': product_link['href'],
+							'brand': product_link['data-brand'],
+							'store': "Deserres"
+						}
+					else:
+						print("error")
+						print(product_link.contents)
+						break
+print(product_map)
+print("Done Deserres!!")
+#Save our results
+f = open("deserres.json","w+")
+f.write(json.dumps(product_map.values()))
+f.close()
+
